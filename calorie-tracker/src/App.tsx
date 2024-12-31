0cba37bf81a3900213adc2f746278a9883d082d9
alvarogonmar <alvarogonmar0@gmail.com>
2024-12-30 21:38:16 -0600

    refactor App component header for improved styling and layout

--- calorie-tracker/src/App.tsx
+++ calorie-tracker/src/App.tsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <h1 className="text-6xl">Calorie Tracker</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase ">
+            Calorie Tracker
+          </h1>
+        </div>
+      </header>
     </>
   );
 }
