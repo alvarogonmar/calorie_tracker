de741bc0cb01a7d6815709713b60a26f46ebfdb0
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-02 17:39:49 -0600

    enhance Form component with activity input and update category names to English

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -4,15 +4,26 @@ export default function Form() {
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="category">Category:</label>
+        <label htmlFor="category" className="font-bold">
+          Category:
+        </label>
         <select
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
         >
           {categories.map((category) => (
-            <option value="">{category.name}</option>
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
           ))}
         </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">
+          Activity:
+        </label>
+        <input id="activity" type="text" />
       </div>
     </form>
   );
