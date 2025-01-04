923bb53372df6dff05673b1a5a1043dd2eb6b8a3
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-03 21:40:15 -0600

    feat(ActivityList): add category name display using memoization for improved performance

--- calorie-tracker/src/components/ActivityList.tsx
+++ calorie-tracker/src/components/ActivityList.tsx
@@ -1,10 +1,17 @@
 import { Activity } from "../types";
+import { categories } from "../data/categories";
+import { useMemo } from "react";
 
 type ActivityListProps = {
   activities: Activity[];
 };
 
 export default function ActivityList({ activities }: ActivityListProps) {
+  const categoryName = useMemo(
+    () => (category: Activity["category"]) =>
+      categories.map((cat) => (cat.id === category ? cat.name : "")),
+    [activities]
+  );
   return (
     <>
       <h2 className="text-4xl font-bold text-slate-600 text-center">
@@ -17,7 +24,7 @@ export default function ActivityList({ activities }: ActivityListProps) {
           className="px-5 py-10 bg-white mt-5 flex justify-between"
         >
           <div className="space-y-2 relative">
-            <p>{activity.category}</p>
+            <p>{categoryName(+activity.category)}</p>
             <p className="text-2xl font-bold pt-5">{activity.name}</p>
             <p className="font-black text-4xl text-lime-500">
               {activity.calories} {""}
