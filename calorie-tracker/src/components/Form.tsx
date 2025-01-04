d305165708c892ef99d0e7d7248e9f8799bacfb0
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-03 20:30:57 -0600

    feat(Form): add unique identifier to activity state and reset on submission

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -8,7 +8,8 @@ type FormProps = {
   dispatch: Dispatch<ActivityActions>;
 };
 
-const initialState = {
+const initialState: Activity = {
+  id: uuidv4(),
   category: 1,
   name: "",
   calories: 0,
@@ -36,7 +37,10 @@ export default function Form({ dispatch }: FormProps) {
     e.preventDefault();
     dispatch({ type: "save-activity", payload: { newActivity: activity } });
 
-    setActivity(initialState);
+    setActivity({
+      ...initialState,
+      id: uuidv4(),
+    });
   };
 
   return (
