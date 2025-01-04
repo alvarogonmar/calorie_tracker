3357e81be0a82fe21da7b4e0fee95bc6003f5176
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-03 22:16:19 -0600

    feat(ActivityList): pass dispatch to ActivityList for handling actions

--- calorie-tracker/src/components/ActivityList.tsx
+++ calorie-tracker/src/components/ActivityList.tsx
@@ -1,10 +1,12 @@
 import { Activity } from "../types";
 import { categories } from "../data/categories";
-import { useMemo } from "react";
+import { Dispatch, useMemo } from "react";
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { ActivityActions } from "../reducers/activity-reducer";
 
 type ActivityListProps = {
   activities: Activity[];
+  dispatch: Dispatch<ActivityActions>;
 };
 
 export default function ActivityList({ activities }: ActivityListProps) {
